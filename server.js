7726934f202f1670b31198c059959376243f50ba
DA-Mac-M1-73 <da-mac-m1-73@DA-Mac73s-MacBook-Air-73.local>
2023-04-12 15:40:30 +0200

    second  commit

--- server.js
+++ server.js
@@ -34,8 +34,9 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
+require("./app/routes/tutorial.routes")(app);
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
